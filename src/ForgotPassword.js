6e5004158aefef43f3e565547d196eec5da374fd
MonikaPandian <panmonikmm@gmail.com>
2022-10-18 21:11:40 +0530

    crm-app-react

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,12 +1,13 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
+import validator from 'validator';
 
 const ForgotPassword = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState("")
-      
+    
     return (
         <div>
             {/* <!-- As a heading --> */}
@@ -14,20 +15,20 @@ const ForgotPassword = () => {
                 <div className="container-fluid">
                     <a className="navbar-brand text-white">CRM Application</a>
                     <form className="d-flex" role="search">
-                        <button onClick={()=>navigate("/login")} className="btn btn-light" type="submit">Login</button>&nbsp;
-                        <button onClick={()=>navigate("/signup")} className="btn btn-light" type="submit">Signup</button>
+                        <button onClick={() => navigate("/login")} className="btn btn-light" type="submit">Login</button>&nbsp;
+                        <button onClick={() => navigate("/signup")} className="btn btn-light" type="submit">Signup</button>
                     </form>
                 </div>
-            </nav>           
+            </nav>
 
-            <div className="forgot-password">
-                <h4 className='heading-text'>Reset your password</h4>
+            <div className="forgot-password">             
+              <h4 className='heading-text'>Reset your password</h4>
                 <TextField required className="login-text-email" onChange={(event) => setUsername(event.target.value)}
-                    label="Email" value={username} variant="standard" />                
-                <Button className="login-button" 
+                    label="Email" value={username} variant="standard" />                    
+                <Button className="login-button"
                     variant="contained" onClick={() => {
                         const employeeFromDB = {
-                            username: username,                           
+                            username: username,
                         };
                         fetch("https://customer-relation-manage-app.herokuapp.com/employees/send-email", {
                             method: "POST",
@@ -37,16 +38,16 @@ const ForgotPassword = () => {
                             }
                         })
                             .then((data) => data.json())
-                            .then((data) => { 
+                            .then((data) => {
                                 if (data.message == "success") {
-                                   window.alert("success!! Password reset link has been sent to your mail")                                
-                                } 
+                                    window.alert("success!! Password reset link has been sent to your mail")
+                                }
                                 else {
                                     window.alert("Please enter valid and registered email ID")
                                 }
                             })
 
-                    }}>Send password reset email</Button>                                                                                                              
+                    }}>Send password reset email</Button>                          
             </div>
         </div>
     )
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -5,67 +5,70 @@ import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
     const navigate = useNavigate();
-const [username, setUsername] = useState("")
-const [firstName, setFirstName] = useState("")
-const [lastName, setLastName] = useState("")
-const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
 
-return (
-    <div>
-        {/* <!-- As a heading --> */}
-        <nav className="navbar bg-primary">
-            <div className="container-fluid">
-                <a className="navbar-brand text-white">CRM Application</a>
-                <form className="d-flex" role="search">
-                    <button onClick={() => navigate("/login")} className="btn btn-light" type="submit">Login</button>&nbsp;
-                    <button onClick={() => navigate("/signup")} className="btn btn-light" type="submit">Signup</button>
-                </form>
-            </div>
-        </nav>
+    return (
+        <div>
+            {/* <!-- As a heading --> */}
+            <nav className="navbar bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-white">CRM Application</a>
+                    <form className="d-flex" role="search">
+                        <button onClick={() => navigate("/login")} className="btn btn-light" type="submit">Login</button>&nbsp;
+                        <button onClick={() => navigate("/signup")} className="btn btn-light" type="submit">Signup</button>
+                    </form>
+                </div>
+            </nav>
 
-        <div className="login">
-            <h4 className='heading-text'>Please sign in to continue</h4>
-            <TextField required className="login-text-email" onChange={(event) => setUsername(event.target.value)}
-                label="Email" value={username} variant="standard" />
-            <TextField required className="login-text-fname" onChange={(event) => setFirstName(event.target.value)}
-                label="First name" value={firstName} variant="standard" />
-            <TextField required onChange={(event) => setLastName(event.target.value)}
-                label="Last name" value={lastName} className="login-text-lname" variant="standard" />
-            <TextField required type="password" onChange={(event) => setPassword(event.target.value)}
-                label="password" value={password} className="login-text-password" variant="standard" />
-            <p className='forgot-password-text'><a style={{ textDecoration: "underline", color: "blue" }} onClick={() => { navigate("/forgot-password") }}>Forgot Password ?</a></p>
+            <div className="login">
+                <form>
+                <h4 className='heading-text'>Please sign in to continue</h4>
+                <TextField required className="login-text-email" onChange={(event) => setUsername(event.target.value)}
+                    label="Email" value={username} variant="standard" />
+                <TextField required className="login-text-fname" onChange={(event) => setFirstName(event.target.value)}
+                    label="First name" value={firstName} variant="standard" />
+                <TextField required onChange={(event) => setLastName(event.target.value)}
+                    label="Last name" value={lastName} className="login-text-lname" variant="standard" />
+                <TextField required type="password" onChange={(event) => setPassword(event.target.value)}
+                    label="password" value={password} className="login-text-password" variant="standard" />
+                <p className='forgot-password-text'><a style={{ textDecoration: "underline", color: "blue" }} onClick={() => { navigate("/forgot-password") }}>Forgot Password ?</a></p>
 
-            <Button className="login-button"
-                variant="contained" onClick={() => {
-                    const employeeFromDB = {
-                        username: username,
-                        firstName: firstName,
-                        lastName: lastName,
-                        password: password
-                    };
-                    fetch("https://customer-relation-manage-app.herokuapp.com/employees/login", {
-                        method: "POST",
-                        body: JSON.stringify(employeeFromDB),
-                        headers: {
-                            "Content-Type": "application/json",
-                        }
-                    })
-                        .then((data) => data.json())
-                        .then((data) => {
-                            if (data.message == "Successful login") {
-                                navigate("/dashboard")
-                            }
-                            else {
-                                window.alert("Please check your username and password. If new user please sign up to create an account")
+                <Button className="login-button"
+                    variant="contained" onClick={() => {
+                        const employeeFromDB = {
+                            username: username,
+                            firstName: firstName,
+                            lastName: lastName,
+                            password: password
+                        };
+                        fetch("https://customer-relation-manage-app.herokuapp.com/employees/login", {
+                            method: "POST",
+                            body: JSON.stringify(employeeFromDB),
+                            headers: {
+                                "Content-Type": "application/json",
                             }
                         })
+                            .then((data) => data.json())
+                            .then((data) => {
+                                if (data.message == "Successful login") {
+                                    navigate("/dashboard")
+                                }
+                                else {
+                                    window.alert("Please check your username and password. If new user please sign up to create an account")
+                                }
+                            })
 
-                }} >Login</Button>
-            <p className='sign-up-text'>Don't have an account ? <a style={{ textDecoration: "underline", color: "blue" }} onClick={() => { navigate("/signup") }}>Sign Up</a></p>
+                    }} >Login</Button>
+                <p className='sign-up-text'>Don't have an account ? <a style={{ textDecoration: "underline", color: "blue" }} onClick={() => { navigate("/signup") }}>Sign Up</a></p>
+                </form>                
+            </div>
         </div>
-    </div>
-)}
-   
+    )
+}
+
 export default LoginForm;
 
 
